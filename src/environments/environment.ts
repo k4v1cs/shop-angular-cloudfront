277ae8796ab71aea4900691355bcb4a5982c8a2a
Katalin Balint <Katalin.Balint.external@stepstone.com>
2024-04-03 11:51:40 +0200

    feat: added import endpoint, enabled import

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,14 +9,14 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://ny8ieu19k1.execute-api.eu-central-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://0e86mwnks8.execute-api.eu-central-1.amazonaws.com/dev',
     bff: 'https://ny8ieu19k1.execute-api.eu-central-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: true,
     order: false,
-    import: false,
+    import: true,
     bff: true,
     cart: false,
   },
